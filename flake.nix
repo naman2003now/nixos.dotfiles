638b5220d1ccd0b290e018e9b352723ea772c960
Naman Agrawal <naman2003now@gmail.com>
2024-04-10 12:04:59 +0530

    renamed: window-managers -> window-manager
    - Because singular makes more sense

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         system = "x86_64-linux";
         modules = [
           ./core
-	  ./window-managers
+	  ./window-manager
 
           # home-manager.nixosModules.home-manager
           # {
