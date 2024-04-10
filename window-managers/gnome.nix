8b3969a0fba7fbb1f53c3ebabe5bcb239a35e611
Naman Agrawal <naman2003now@gmail.com>
2024-04-10 11:51:12 +0530

    feature: Removed bloat from gnome.
    Excluded all the default gnome packages for a cleaner experience.

--- window-managers/gnome.nix
+++ window-managers/gnome.nix
@@ -7,6 +7,32 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Removing Bloat
+  services.xserver.desktopManager.xterm.enable = false;
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    eog         # image viewer
+    epiphany    # web browser
+    simple-scan # document scanner
+    totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    file-roller # archive manager
+    geary       # email client
+    seahorse    # password manager
+
+    pkgs.gnome-tour 
+    gnome-clocks
+    gnome-calendar
+    gnome-calculator
+    gnome-weather
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-system-monitor
+    gnome-terminal
+  ];
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
@@ -15,6 +41,7 @@
   # GNOME autologin workaround
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
+
 
   # Configure keymap in X11
   services.xserver.xkb = {
