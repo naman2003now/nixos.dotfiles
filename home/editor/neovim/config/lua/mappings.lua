2cd19fa5af1cda6fd42d87850aef8b11f7d84793
Naman Agrawal <naman2003now@gmail.com>
2024-04-12 14:33:19 +0530

    neovim: nvim-tree mappings
    - C-n toggle explorer
    - <leader>e focus explorer

--- home/editor/neovim/config/lua/mappings.lua
+++ home/editor/neovim/config/lua/mappings.lua
@@ -1,1 +1,5 @@
 local map = vim.keymap.set
+
+-- nvim-tree mappings
+map("n", "<leader>e", "<cmd>NvimTreeFocus<cr>", { desc = "Focus Explorer" })
+map("n", "C-n", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle Explorer" })
