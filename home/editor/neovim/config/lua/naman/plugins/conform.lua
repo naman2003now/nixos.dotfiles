53a49b76f32629ca3e4fd3cd4dc9eca4d9c41e03
Naman Agrawal <naman2003now@gmail.com>
2024-05-02 15:46:04 +0530

    neovim: added conform for formatting file

---
+++ home/editor/neovim/config/lua/naman/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { { "prettierd", "prettier" } },
+			-- Rust formater
+			rust = { "rustfmt" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
+}
