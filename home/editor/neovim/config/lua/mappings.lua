ef830851a968e09c83981560ffd3609217b85f3f
Naman Agrawal <naman2003now@gmail.com>
2024-05-02 13:12:06 +0530

    neovim: refactor: Changed a few mappings and their desc
    - Lazygit is not under the Open (<leader>o) section
    - Renamed <leader>f to find instead of file

--- home/editor/neovim/config/lua/mappings.lua
+++ home/editor/neovim/config/lua/mappings.lua
@@ -3,12 +3,12 @@ local wk = require("which-key")
 
 wk.register({
 	e = { "<cmd>NvimTreeFocus<cr>", "Focus Explorer" },
-	g = {
-		name = "git",
-		l = { "<cmd>LazyGit<cr>", "Open Lazy Git" },
+	o = {
+		name = "open",
+		g = { "<cmd>LazyGit<cr>", "Open Lazy Git" },
 	},
 	f = {
-		name = "file",
+		name = "find",
 		f = { "<cmd>Telescope find_files<cr>", "Find Files" },
 		w = { "<cmd>Telescope live_grep<cr>", "Global Search" },
 		b = { "<cmd>Telescope buffers<cr>", "Search buffers" },
