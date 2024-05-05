198ceb407bb251b1987a89475d20c920c43f26c2
Naman Agrawal <naman2003now@gmail.com>
2024-05-05 11:22:40 +0530

    feat: enable fish globally

--- core/users.nix
+++ core/users.nix
@@ -1,7 +1,9 @@
 { config, pkgs, inputs, ... }: {
+  programs.fish.enable = true;
   users.users.naman = {
     isNormalUser = true;
     description = "Naman Agrawal";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
   };
 }
